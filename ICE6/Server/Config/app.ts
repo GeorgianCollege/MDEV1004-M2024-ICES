1dca3b5614fce3ac1311cff31e200474f93c44a2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-06-13 18:08:07 -0400

    demonstrated sub-domain level security

--- ICE6/Server/Config/app.ts
+++ ICE6/Server/Config/app.ts
@@ -99,6 +99,8 @@ let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) =>
 passport.use(strategy);
 
 app.use('/api', indexRouter);
+/* Example: Secure the movie routes with JWT authentication */
+//app.use('/api/movie', passport.authenticate('jwt', {session: false}), movieRouter);
 app.use('/api/movie', movieRouter);
 
 // catch 404 and forward to error handler
