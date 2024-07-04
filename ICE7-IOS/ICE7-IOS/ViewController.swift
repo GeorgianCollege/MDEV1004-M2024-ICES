b9bfcede5377053cf32eb46c123ab2eb1183cd2e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-04 11:56:19 -0400

    added Project files for ICE7-IOS

---
+++ ICE7-IOS/ICE7-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICE7-IOS
+//
+//  Created by Tom Tsiliopoulos on 2024-07-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
