9343e642683eeb92610df177055f1b6ba6cb717b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-11 11:22:16 -0400

    added project files for ICE8-Android

---
+++ ICE8-Android/app/src/main/java/ca/georgiancollege/ice8_android/MainActivity.kt
@@ -0,0 +1,34 @@
+package ca.georgiancollege.ice8_android
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.georgiancollege.ice8_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MovieViewModel by viewModels()
+    private lateinit var firstAdapter: FirstAdapter
+    private lateinit var movieList: MutableList<Movie>
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.movies.observe(this) { movies ->
+            movieList = movies.toMutableList()
+            firstAdapter = FirstAdapter(movies)
+
+            binding.FirstRecyclerView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = firstAdapter
+            }
+        }
+
+        viewModel.getAllMovies()
+    }
+}
