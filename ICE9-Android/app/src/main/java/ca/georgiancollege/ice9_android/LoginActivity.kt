b8a4bc6090edb7732357a177384240bac71a0458
ttsiliopoulos <ttsiliopoulos@georgiancollege.ca>
2024-07-18 16:23:08 -0400

    added RegisterActivty logic

--- ICE9-Android/app/src/main/java/ca/georgiancollege/ice9_android/LoginActivity.kt
+++ ICE9-Android/app/src/main/java/ca/georgiancollege/ice9_android/LoginActivity.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import retrofit2.*
 import ca.georgiancollege.ice9_android.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity()
 {
@@ -36,6 +41,47 @@ class LoginActivity : AppCompatActivity()
 
     private fun loginUser(username: String, password: String)
     {
+        val user = User(username = username, password = password)
+        DataManager.instance(this).loginUser(user, object : Callback<ApiResponse<User>>
+        {
+            override fun onResponse(call: Call<ApiResponse<User>>, response: Response<ApiResponse<User>>)
+            {
+                if (response.isSuccessful && response.body()?.success == true)
+                {
+                    println("User Logged In Successfully")
+                    val token = response.body()?.token
 
+                    token?.let {
+                        val editor = sharedPreferences.edit()
+                        editor.putString("auth_token", it)
+                        editor.apply()
+
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finish()
+                    }
+                } else {
+                    println("User Not Logged In")
+                    showLoginFailedSnackbar("Login failed: ${response.body()?.message}")
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse<User>>, t: Throwable)
+            {
+                println("Login Error")
+                showLoginFailedSnackbar("Login error: ${t.message}")
+            }
+        })
+    }
+
+    private fun showLoginFailedSnackbar(message: String) {
+        val snackbar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+        val view = snackbar.view
+        val params = view.layoutParams as FrameLayout.LayoutParams
+
+        params.gravity = Gravity.TOP
+        view.layoutParams = params
+        view.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_red_light))
+
+        snackbar.show()
     }
 }
