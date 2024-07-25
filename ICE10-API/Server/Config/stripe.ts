78541151ea2f8dcdd6dc6082345a80f9239f7a0b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-25 11:57:07 -0400

    fixed bug with API Key

--- ICE10-API/Server/Config/stripe.ts
+++ ICE10-API/Server/Config/stripe.ts
@@ -1,4 +1,4 @@
 export default {
-    secretKey: process.env.STRIP_SECRET_KEY as string,
+    secretKey: process.env.STRIPE_SECRET_KEY as string,
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY as string
 }
