33287b7a2b51104e75e6924fdf4ec004231a9ba6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-08-01 11:17:14 -0400

    added project files for ICE11-Android

---
+++ ICE11Android/app/src/main/java/ca/georgiancollege/ice11_android/MainActivity.kt
@@ -0,0 +1,18 @@
+package ca.georgiancollege.ice11_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.georgiancollege.ice11_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
