1b3d9a8a2fba61c48136c1ab435e3b16e4e5c163
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-08-01 12:36:26 -0400

    added iOS app template

---
+++ ICE11-IOS/ICE11-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICE11-IOS
+//
+//  Created by Tom Tsiliopoulos on 2024-08-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
